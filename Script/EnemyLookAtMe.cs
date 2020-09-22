f9dfe4ffcc7da387c36be3c7e979cc4bdeaaa8c3
chefblogger <70014924+chefblogger@users.noreply.github.com>
2020-09-23 01:03:23 +0200

    Add files via upload

---
+++ Script/EnemyLookAtMe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLookAtMe : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.LookAt(target); 
+    }
+}
